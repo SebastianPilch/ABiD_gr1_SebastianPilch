2a1278655db4c9719a893d5757505bd982b774ee
Sebastian <sebapilch@student.agh.edu.pl>
2022-10-06 17:17:04 +0200

    'cos's

--- AiBD_Lab1.py
+++ AiBD_Lab1.py
@@ -1,14 +1,29 @@
-import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
 
-print('dupa')
 
-
-def fun(x: np.ndarray, f=lambda a: a ** 2 + 5):
+def fun(x, f=lambda a: a ** 2 + 5):
+    y = np.ndarray(shape=[len(x)])
     for i in range(len(x)):
-        return f(x)
+        y[i] = f(x[i])
+    return y
 
-
-x = np.linspace(-5, 5, 100)
-print(x)
-print(fun(x))
+x = np.linspace(-7, 7, 10000)
+plt.plot(x,fun(x),label = 'x (-1,1)')
+plt.grid()
+plt.xlim(-1,1)
+plt.legend()
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('f=x^2+5')
+plt.show()
+plt.plot(x,fun(x),label = 'x (-6,6)')
+plt.grid()
+plt.xlim(-6,6)
+plt.legend()
+plt.show()
+plt.plot(x,fun(x), label = 'x (-5,5)')
+plt.grid()
+plt.xlim(-5,5)
+plt.legend()
+plt.show()
